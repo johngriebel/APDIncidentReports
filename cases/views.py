f6462cefa2be87d33a7391502440f5f80e8fb949
John Griebel <johnkgriebel@gmail.com>
2018-02-01 06:26:56 -0500

    Add very basics of a form to create Incidents
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- cases/views.py
+++ cases/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import IncidentForm
 
 
 # Create your views here.
 def index(request, *args, **kwargs):
     context = {}
     return render(request, "cases/index.html", context=context)
+
+
+def create_incident(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = IncidentForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = IncidentForm()
+
+    context = {'form': form}
+    return render(request, "cases/create_incident.html", context=context)
