ca34bcab8f622bf24ab6566d9ea4ca504d960214
John Griebel <johnkgriebel@gmail.com>
2018-02-08 08:14:39 -0500

    Cleanup search function a bit
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- cases/search.py
+++ cases/search.py
@@ -1,37 +1,48 @@
+from typing import Union
+from datetime import datetime
 from django.utils import timezone
 from .models import Incident
 from .utils import isincident_field, convert_date_string_to_object
+
+
+def cleanse_filter_key(key: str) -> str:
+    if "_min" in key:
+        filter_key = key.replace("_min", "__gte")
+    elif "_max" in key:
+        filter_key = key.replace("_max", "__lte")
+    elif key == "reporting_officer":
+        filter_key = key + "__user__last_name__iexact"
+    elif "earliest_" in key:
+        filter_key = key + "__gte"
+    elif "latest" in key:
+        filter_key = key + "__lte"
+    elif "location" in key:
+        filter_key = key.replace("location_", "location__") + "__icontains"
+    elif key == "offenses":
+        filter_key = key + "__in"
+    else:
+        filter_key = key
+    return filter_key
+
+
+def cleanse_value(key:str, data:dict):
+    if "datetime" in key:
+        value = convert_date_string_to_object(data[key])
+        value = timezone.make_aware(value)
+    elif key == "offenses":
+        value = data.getlist(key)
+    else:
+        value = data[key]
+    return value
 
 
 def get_search_results(params: dict):
     filter_dict = {}
     for key in params:
         if isincident_field(key):
-            if "_min" in key:
-                filter_key = key.replace("_min", "__gte")
-            elif "_max" in key:
-                filter_key = key.replace("_max", "__lte")
-            elif key == "reporting_officer":
-                filter_key = key + "__user__last_name__iexact"
-            elif "earliest_" in key:
-                filter_key = key + "__gte"
-            elif "latest" in key:
-                filter_key = key + "__lte"
-            elif "location" in key:
-                filter_key = key.replace("location_", "location__") + "__icontains"
-            elif key == "offenses":
-                filter_key = key + "__in"
-            else:
-                filter_key = key
+            filter_key = cleanse_filter_key(key)
             if params[key]:
-                if "datetime" in key:
-                    value = convert_date_string_to_object(params[key])
-                    value = timezone.make_aware(value)
-                elif key == "offenses":
-                    value = params.getlist(key)
-                else:
-                    value = params[key]
-                filter_dict[filter_key] = value
+                filter_dict[filter_key] = cleanse_value(key, params)
     print(f"Filter Dict: {filter_dict}")
     incidents = Incident.objects.filter(**filter_dict)
     print(f"Incidents: {incidents}")
