bf79da2f6e89444d457ec5239b8878804efe7406
John Griebel <johnkgriebel@gmail.com>
2018-02-27 12:50:42 -0500

    Creation and reading of Incidents, officers, offenses working
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- cases/rest_views.py
+++ cases/rest_views.py
@@ -1,5 +1,9 @@
+import logging
+import json
+from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
+from address.models import _to_python
 from .models import (Officer, Incident,
                      IncidentInvolvedParty,
                      IncidentFile)
@@ -8,6 +12,8 @@ from .serializers import (OfficerSerializer, IncidentSerializer,
                           IncidentFileSerializer)
 from .constants import VICTIM, SUSPECT
 from rest_framework import viewsets
+logger = logging.getLogger('cases')
+User = get_user_model()
 
 
 class OfficerViewSet(viewsets.ModelViewSet):
@@ -18,6 +24,24 @@ class OfficerViewSet(viewsets.ModelViewSet):
 class IncidentViewSet(viewsets.ModelViewSet):
     queryset = Incident.objects.all()
     serializer_class = IncidentSerializer
+
+    def create(self, request, *args, **kwargs):
+        logger.debug(request.data)
+        dirty_data = {key: value for key, value in request.data.items()}
+        logger.debug(f"Dirty data: {dirty_data}")
+        for field in dirty_data:
+            if "officer" in field or "supervisor" in field:
+                dirty_data[field] = Officer.objects.get(id=dirty_data[field])
+        dirty_data['offenses'] = json.loads(dirty_data['offenses'])
+        dirty_data['location'] = json.loads(dirty_data['location'])
+        serializer = self.get_serializer(data=request.data)
+        # logger.debug(f"Type of data['offenses']: {type(json.loads(dirty_data['offenses']))}")
+        logger.debug(f"Valid? {serializer.is_valid()}")
+        logger.debug(serializer.validated_data)
+        logger.debug(f"Errors: {serializer.errors}")
+        serializer.create(validated_data=dirty_data)
+        return Response(status=status.HTTP_201_CREATED,
+                        data=serializer.data)
 
 
 class VictimViewSet(viewsets.ModelViewSet):
--- cases/serializers.py
+++ cases/serializers.py
@@ -1,9 +1,19 @@
+import logging
 from django.contrib.auth import get_user_model
+from address.models import _to_python, Address
 from rest_framework import serializers
+from rest_framework.settings import api_settings
 from .models import (Officer, Incident,
                      Offense, IncidentInvolvedParty,
                      IncidentFile)
 User = get_user_model()
+logger = logging.getLogger('cases')
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,15 +24,47 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class OfficerSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    def to_internal_value(self, data):
+        try:
+            return Officer.objects.get(id=data)
+        except Officer.DoesNotExist:
+            logger.debug(f"Tried to find officer with ID: {data}")
+            message = self.error_messages['invalid'].format(
+                datatype=type(data).__name__
+            )
+            raise serializers.ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
 
     class Meta:
         model = Officer
         fields = ("id", "officer_number", "supervisor", "user")
         read_only_fields = ("id",)
 
+    def create(self, validated_data):
+        logger.debug(validated_data)
+        officer = Officer.objects.create(user=validated_data['user'],
+                                         officer_number=validated_data['officer_number'],
+                                         supervisor=validated_data['supervisor'])
+        return officer
+
 
 class OffenseSerializer(serializers.ModelSerializer):
+
+    def to_internal_value(self, data):
+        try:
+            return Offense.objects.get(id=data)
+        except Offense.DoesNotExist:
+            logger.debug(f"Tried to find offense with ID: {data}")
+            message = self.error_messages['invalid'].format(
+                datatype=type(data).__name__
+            )
+            raise serializers.ValidationError({
+                api_settings.NON_FIELD_ERRORS_KEY: [message]
+            }, code='invalid')
+
     class Meta:
         model = Offense
         fields = "__all__"
@@ -35,6 +77,26 @@ class IncidentSerializer(serializers.ModelSerializer):
     investigating_officer = OfficerSerializer()
     officer_making_report = OfficerSerializer()
     supervisor = OfficerSerializer()
+    location = AddressSerializer()
+
+    def validate_location(self, value):
+        for addr_field in ["raw", "country", "country_code", "state",
+                           "state_code", "postal_code", "street_number",
+                           "route", "locality"]:
+            if addr_field not in value:
+                raise serializers.ValidationError(f"Missing field {addr_field} from location.")
+        address_obj = _to_python(value)
+        return address_obj
+
+    def create(self, validated_data):
+        logger.debug("in the serializer's create method")
+        logger.debug(validated_data)
+        offenses = validated_data.pop("offenses")
+        incident = Incident.objects.create(**validated_data)
+        offense_objects = Offense.objects.filter(id__in=offenses)
+        for offense in offense_objects:
+            incident.offenses.add(offense)
+        return incident
 
     class Meta:
         model = Incident
