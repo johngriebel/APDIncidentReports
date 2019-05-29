57be182947c9b33c3f41cb40e0901311d47fa9cc
John Griebel <john@johngriebel.com>
2019-05-28 21:40:25 -0400

    [APD-13] * Fix complexity and styling issues in test utils
    Signed-off-by: John Griebel <john@johngriebel.com>

--- cases/tests/utils.py
+++ cases/tests/utils.py
@@ -1,7 +1,10 @@
 import os
 import random
 import logging
-from typing import Dict, Optional, Any
+from typing import (Dict,
+                    List,
+                    Any,
+                    Tuple)
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from rest_framework_jwt.settings import api_settings
@@ -22,6 +25,9 @@ User = get_user_model()
 jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
 
+
+def _get_db_vals_for_choice_list(choices: List[Tuple]) -> List[str]:
+    return [c[0] for c in choices]
 
 def generate_jwt_for_tests(user: User):
     payload = jwt_payload_handler(user)
@@ -68,38 +74,38 @@ class IncidentDataFaker:
     def generate_narrative(self) -> str:
         return self.fake.paragraph()
 
-    def generate_involved_party(self, *, party_type: str,
-                                incident: Optional[Incident]=None,
-                                officer_signed: Optional[Officer]=None) -> Dict:
+    def generate_involved_party(self, party_type: str,
+                                incident: Incident,
+                                officer_signed: Officer) -> Dict:
+
         party_data = {'first_name': self.fake.first_name(),
                       'last_name': self.fake.last_name(),
-                      'incident': incident.id if incident
-                                              else IncidentFactory().id,
-                      'officer_signed': officer_signed.id if officer_signed
-                                                          else OfficerFactory().id,
+                      'incident': incident.id,
+                      'officer_signed': officer_signed.id,
                       'party_type': party_type,
                       'juvenile': self.fake.pybool(),
                       'home_address': self._maybe(to_return=self.generate_address()),
                       'date_of_birth': self.fake.past_date(),
                       'sex': random.choice([MALE, FEMALE]),
-                      'race': random.choice([r[0] for r in RACE_CHOICES]),
+                      'race': random.choice(_get_db_vals_for_choice_list(choices=RACE_CHOICES)),
                       'height': random.randint(24, 90),
                       'weight': random.randint(2, 400),
-                      'hair_color': random.choice([h[0] for h in HAIR_COLOR_CHOICES]),
-                      'eye_color': random.choice([e[0] for e in EYE_COLOR_CHOICES]),
+                      'hair_color': random.choice(_get_db_vals_for_choice_list(choices=HAIR_COLOR_CHOICES)),
+                      'eye_color': random.choice(_get_db_vals_for_choice_list(choices=EYE_COLOR_CHOICES)),
                       'build': self.fake.text(max_nb_chars=25),
                       'tattoos': self.fake.text(max_nb_chars=30),
                       'scars': self._maybe(to_return=self.fake.text(max_nb_chars=30)),
                       'hairstyle': self._maybe(to_return=self.fake.text(max_nb_chars=30))}
 
         drivers_license = self._maybe(to_return=self.fake.text(max_nb_chars=100))
+
         if drivers_license:
-            logger.debug(f"State choices: {[s[0] for s in STATE_CHOICES]}")
-            dl_state = random.choice([s[0] for s in STATE_CHOICES])
+            dl_state = random.choice(_get_db_vals_for_choice_list(choices=STATE_CHOICES))
             party_data['drivers_license'] = drivers_license
             party_data['drivers_license_state'] = dl_state
 
         employer = self._maybe(to_return=self.fake.company())
+
         if employer:
             employer_address = self.generate_address()
             party_data['employer'] = employer
@@ -135,8 +141,8 @@ class IncidentDataFaker:
 
 
 def generate_random_file_content(suffix: str,
-                                 num_kb: int=2,
-                                 base_path: str=None) -> SimpleUploadedFile:
+                                 num_kb: int = 2,
+                                 base_path: str = None) -> SimpleUploadedFile:
     fname = "test_file_" + str(suffix)
     full_path = os.path.join(base_path or "/tmp/", fname)
     if os.path.isfile(full_path):
